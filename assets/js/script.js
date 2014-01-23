d748b6b3d2eca107c0b549ff80d834c6b9b30d5e
Chris Socha <cschlumb@thoughtworks.com>
2014-01-23 16:22:34 +0000

    Lets make JSHint happy...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,36 @@
 $(window).bind("ready", function() {
-	
-	self = this;
 
-	self.titleWidth = $(".title").outerWidth();
-	self.navigationWidth = $(".navigation").outerWidth();
-	self.mobileWidth = titleWidth + navigationWidth + 20;
+  var titleWidth = $(".title").outerWidth();
+  var navigationWidth = $(".navigation").outerWidth();
+  var mobileWidth = titleWidth + navigationWidth + 20;
 
-	var isMobile = null;
+  var isMobile = null;
 
-	if (matchMedia){
-		var mobileMedia = window.matchMedia( "(max-width: " + mobileWidth + "px)" );
-		mobileMedia.addListener(IsMobileCallback);
-		IsMobileCallback(mobileMedia);
-	}
+  if (matchMedia){
+    var mobileMedia = window.matchMedia( "(max-width: " + mobileWidth + "px)" );
+    mobileMedia.addListener(IsMobileCallback);
+    IsMobileCallback(mobileMedia);
+  }
 
-	function IsMobileCallback(mobileMedia){
-		self.isMobile = mobileMedia.matches;
-		
-		if (self.isMobile) {
-			$(".navigation-link").addClass("hide");
-			$(".mobile-navigation").addClass("showMobileNavigation");
-		} else{
-			$(".navigation-link").removeClass("hide");
-			$(".mobile-navigation").removeClass("showMobileNavigation");
-		}
-	}
+  function IsMobileCallback(mobileMedia){
+    isMobile = mobileMedia.matches;
+    
+    if (isMobile) {
+      $(".navigation-link").addClass("hide");
+      $(".mobile-navigation").addClass("showMobileNavigation");
+    } else{
+      $(".navigation-link").removeClass("hide");
+      $(".mobile-navigation").removeClass("showMobileNavigation");
+    }
+  }
 
-	$(".navigation").hover(function(){
-		if (self.isMobile) {
-			$(".navigation-link").addClass("showDropdown");
-		}
-	}, function(){
-		$(".navigation-link").removeClass("showDropdown");
-	});
+  $(".navigation").hover(function(){
+    if (isMobile) {
+      $(".navigation-link").addClass("showDropdown");
+    }
+  }, function(){
+    $(".navigation-link").removeClass("showDropdown");
+  });
 
 });
+
