5ec16fc0fde299f1259c93a1f49f34d4f324c2f8
Chris Socha <cschlumb@thoughtworks.com>
2014-09-15 22:28:09 +0100

    Changed to Foundation framework, and other misc changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,6 @@
-window.onerror=function(msg){
-  if (!sessionStorage.jsError){
-    sessionStorage.jsError = "";
-  }
-
-  sessionStorage.jsError += msg + "; ";
-}
-
 $(window).bind("ready", function() {
 
-  var titleWidth = $(".title").outerWidth();
-  var navigationWidth = $(".navigation").outerWidth();
-  var mobileWidth = titleWidth + navigationWidth + 20;
-
-  var isMobile = null;
-
-  if (matchMedia){
-    var mobileMedia = window.matchMedia( "(max-width: " + mobileWidth + "px)" );
-    mobileMedia.addListener(IsMobileCallback);
-    IsMobileCallback(mobileMedia);
-  }
-
-  function IsMobileCallback(mobileMedia){
-    isMobile = mobileMedia.matches;
-    
-    if (isMobile) {
-      $(".navigation-link").addClass("hide");
-      $(".mobile-navigation").addClass("showMobileNavigation");
-    } else{
-      $(".navigation-link").removeClass("hide");
-      $(".mobile-navigation").removeClass("showMobileNavigation");
-    }
-  }
-
-  $(".navigation").hover(function(){
-    if (isMobile) {
-      $(".navigation-link").addClass("showDropdown");
-    }
-  }, function(){
-    $(".navigation-link").removeClass("showDropdown");
-  });
+  // Do nothing
 
 });
 
@@ -66,6 +28,3 @@ function deadPixel(pixelSize){
   console.log("Added dead pixel at " + x + ", " + y);
   
 }
-
-throw "This is a test error"
-
--- readme.md
+++ readme.md
@@ -3,4 +3,8 @@
 
 This is the source for personal blog. It uses [Jekyll](http://jekyllrb.com/) to create static pages, and [SASS](http://sass-lang.com/) for CSS.
 
-Since [GitHub Pages](http://pages.github.com/) does not allow for custom plugins, we must compile SASS to CSS before it is pushed to the repo - This is done by running `ruby SassConverter.rb`.
+Todo:
+
+* Commit back changes to Foundation
+* Add JS Module loader (require.js)
+* Add cookie support to JS Dead Pixel post
